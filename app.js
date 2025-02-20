e990ac9aaef89982d714defd2c41b93f7bea602a
zhao-momo <zhaowenmo_xxs@163.com>
2025-02-20 20:29:27 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+/**
+ * Created by Hongcai Deng on 2015/12/29.
+ */
+
+$(function(){
+  $('.ui.modal')
+    .modal()
+  ;
+
+  var clipboard = new Clipboard('.copyable');
+
+  $customShortId = $('#customShortid');
+  $shortId = $('#shortid');
+  $customTheme = 'check';
+  $placeholder_old = '请等待分配临时邮箱';
+  $placeholder_new = '请输入不带后缀邮箱';
+  $customShortId.on('click',function() {
+    var self = $(this);
+    var editEnable = true;
+    $shortId.prop('disabled', false);
+    if(self.hasClass('edit')) {
+      $shortId.val('');
+      self.removeClass('edit');
+      self.toggleClass($customTheme);
+      $shortId.prop('placeholder', $placeholder_new);
+    } else {
+      $shortId.prop('disabled', true);
+      self.removeClass('check');
+      self.toggleClass('edit');
+      $shortId.prop('placeholder',$placeholder_old);
+      $mailUser = $shortId.val();
+      var mailaddress = $mailUser + '@' + location.hostname;
+      setMailAddress($mailUser);
+      $shortId.val(mailaddress);
+      window.location.reload();
+    }
+  });
+  
+  
+  $maillist = $('#maillist');
+
+  $maillist.on('click', 'tr', function() {
+    var mail = $(this).data('mail');
+    $('#mailcard .header').text(mail.headers.subject || '无主题');
+    $('#mailcard .content:last').html(mail.html);
+    $('#mailcard i').click(function() {
+      $('#raw').modal('show');
+    });
+    $('#raw .header').text('RAW');
+    $('#raw .content').html($('<pre>').html($('<code>').addClass('language-json').html(JSON.stringify(mail, null, 2))));
+    Prism.highlightAll();
+  });
+
+  var socket = io();
+
+  var setMailAddress = function(id) {
+    localStorage.setItem('shortid', id);
+    var mailaddress = id + '@' + location.hostname;
+    $('#shortid').val(mailaddress).parent().siblings('button').find('.mail').attr('data-clipboard-text', mailaddress);
+  };
+
+  $('#refreshShortid').click(function() {
+    socket.emit('request shortid', true);
+  });
+
+  socket.on('connect', function() {
+    if(('localStorage' in window)) {
+      var shortid = localStorage.getItem('shortid');
+      if(!shortid) {
+        socket.emit('request shortid', true);
+      }
+      else {
+        socket.emit('set shortid', shortid);
+      }
+    }
+  });
+
+  socket.on('shortid', function(id) {
+    setMailAddress(id);
+  });
+
+  socket.on('mail', function(mail) {
+    if(('Notification' in window)) {
+      if(Notification.permission === 'granted') {
+        new Notification('New mail from ' + mail.headers.from);
+      }
+      else if(Notification.permission !== 'denied') {
+        Notification.requestPermission(function(permission) {
+          if(permission === 'granted') {
+            new Notification('New mail from ' + mail.headers.from);
+          }
+        })
+      }
+    }
+    $tr = $('<tr>').data('mail', mail);
+    $tr
+      .append($('<td>').text(mail.headers.from))
+      .append($('<td>').text(mail.headers.subject || '无主题'))
+      .append($('<td>').text((new Date(mail.headers.date)).toLocaleTimeString()));
+    $maillist.prepend($tr);
+  });
+});
